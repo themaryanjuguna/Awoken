be56e5ec455917036c2bd143671fbd45b791f836
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-14 23:04:06 +0300

    HttpServer Driver class created

---
+++ src/main/java/org/themaryanjuguna/httpserver/HttpServer.java
@@ -0,0 +1,11 @@
+package org.themaryanjuguna.httpserver;
+
+//Driver class with main method for the HttpServer
+// running configuration
+public class HttpServer {
+    public static void main(String[] args) {
+
+        System.out.println("Server Started...");
+
+    }
+}
