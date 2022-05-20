c5c2dfeb3a9e6945ff7c70d91e84bb9b5d9fd14b
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-20 10:12:55 +0300

    updated user Daos and Sql2o

---
+++ target/heroku/app/target/classes/create.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE awoken;
+\c awoken;
+
+CREATE TABLE users(id serial PRIMARY KEY, employeeId int, name varchar, profile varchar, position varchar, role varchar);
+
+CREATE TABLE news(id serial PRIMARY KEY, title varchar, content varchar, employeeId int);
+
+CREATE TABLE departments(id serial PRIMARY KEY, name varchar, profile varchar, deptId int);
+
+CREATE DATABASE awoken_test WITH TEMPLATE awoken;
